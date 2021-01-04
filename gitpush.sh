26e495257beaf87d1e18682690a63e031d41623e
Stefan Waldherr <stefan@waldherr.org>
2021-01-04 23:23:00 +0100

    config files on 04-01-2021 23:23:00

--- gitpush.sh
+++ gitpush.sh
@@ -5,5 +5,5 @@ git status
 # Commit changes with message with current date stamp
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 # Push changes towards GitHub
-git push -u origin master
+git push -u origin main
 exit
