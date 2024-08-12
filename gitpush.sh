e1249ded26b3be6bcc6cc3e114f75b22a66845b3
root <root@homeassistant.local.hass.io>
2024-08-12 07:52:23 +0200

    config files on 12-08-2024 07:52:23

--- gitpush.sh
+++ gitpush.sh
@@ -6,9 +6,9 @@ scp  192.168.178.143:/etc/evcc.yaml .
 # sanitize
 egrep -v '^#|^\s+#' evcc.yaml |grep . |sed -E 's/(password|user|token|url|host|broker|accessToken|refreshToken|vin|title).*/\1: *****/' > evcc-sanitized.yaml
 git add .
-# git status
+git status
 # Commit changes with message with current date stamp
 git commit -m "config files on `date +'%d-%m-%Y %H:%M:%S'`"
 # Push changes towards GitHub
 git push -u origin main
-exit
+# exit
